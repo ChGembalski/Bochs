b284816f89fabc475ae2fc4ce52d1460ece9b65d
Christoph Gembalski <christoph@gembalski.de>
2024-01-30 19:00:30 +0100

    add regs

--- bochs/gui/new_dbg.cc
+++ bochs/gui/new_dbg.cc
@@ -32,9 +32,83 @@
   #include "siminterface.h"
 
   extern void new_dbg_handler_custom(bool init);
+  extern bx_dbg_gui_c * bx_dbg_new;
+  extern bx_list_c * root_param;
 
   static bxevent_handler old_callback = NULL;
   static void * old_callback_arg = NULL;
+
+
+
+  const char * dbg_regnames[] = {
+#if BX_SUPPORT_X86_64 == 0
+    "EAX", "EBX", "ECX", "EDX", "ESI", "EDI", "EBP", "ESP", "EIP",
+#else
+    "RAX", "RBX", "RCX", "RDX", "RSI", "RDI", "RBP", "RSP", "RIP",
+    "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
+#endif
+    "EFLAGS",
+    "CS.selector", "DS.selector", "ES.selector", "FS.selector", "GS.selector",
+
+    "GDTR.base", "GDTR.limit", "IDTR.base", "IDTR.limit",
+    "LDTR.base", "TR.base",
+
+    "CR0", "CR2", "CR3",
+#if BX_CPU_LEVEL >= 5
+    "CR4",
+#endif /* BX_CPU_LEVEL >= 5 */
+
+#if BX_CPU_LEVEL >= 6
+    "MSR.EFER",
+#endif /* BX_CPU_LEVEL >= 6 */
+
+#if BX_SUPPORT_FPU
+    "FPU.st0.fraction", "FPU.st0.exp", "FPU.st1.fraction", "FPU.st1.exp",
+    "FPU.st2.fraction", "FPU.st2.exp", "FPU.st3.fraction", "FPU.st3.exp",
+    "FPU.st4.fraction", "FPU.st4.exp", "FPU.st5.fraction", "FPU.st5.exp",
+    "FPU.st6.fraction", "FPU.st6.exp", "FPU.st7.fraction", "FPU.st7.exp",
+#endif /* BX_SUPPORT_FPU */
+
+#if BX_CPU_TEST_REGISTER
+#if BX_CPU_LEVEL >= 3 && BX_CPU_LEVEL <= 6
+    "TR3", "TR4", "TR5",
+#if BX_CPU_LEVEL >= 4
+    "TR6", "TR7",
+#endif /* BX_CPU_LEVEL >= 4 */
+#endif /* BX_CPU_LEVEL >= 3 && BX_CPU_LEVEL <= 6 */
+#endif /* BX_CPU_TEST_REGISTER */
+
+#if BX_CPU_LEVEL >= 6
+
+    "SSE.xmm00_0", "SSE.xmm01_0", "SSE.xmm02_0", "SSE.xmm03_0", "SSE.xmm04_0",
+    "SSE.xmm05_0", "SSE.xmm06_0", "SSE.xmm07_0",
+#if BX_SUPPORT_X86_64
+    "SSE.xmm08_0", "SSE.xmm09_0", "SSE.xmm10_0", "SSE.xmm11_0", "SSE.xmm12_0",
+    "SSE.xmm13_0", "SSE.xmm14_0", "SSE.xmm15_0",
+#endif /* BX_SUPPORT_X86_64 */
+
+    "SSE.xmm00_1", "SSE.xmm01_1", "SSE.xmm02_1", "SSE.xmm03_1", "SSE.xmm04_1",
+    "SSE.xmm05_1", "SSE.xmm06_1", "SSE.xmm07_1",
+#if BX_SUPPORT_X86_64
+    "SSE.xmm08_1", "SSE.xmm09_1", "SSE.xmm10_1", "SSE.xmm11_1", "SSE.xmm12_1",
+    "SSE.xmm13_1", "SSE.xmm14_1", "SSE.xmm15_1",
+#endif /* BX_SUPPORT_X86_64 */
+
+#endif /* BX_CPU_LEVEL >= 6 */
+
+    "DR0", "DR1", "DR2", "DR3", "DR6", "DR7",
+
+    NULL
+
+  };
+
+
+
+
+
+
+
+
 
 
   // internal callback handler
@@ -57,14 +131,21 @@
     assert (old_callback != NULL);
     SIM->set_notify_callback(new_dbg_notify_callback, NULL);
 
+    // init internal
+    bx_dbg_new->init_internal();
+
   }
 
   // internal cleanup
   void CloseDebugDialog() {
+
+    // TODO : write debugger configuration
+
     new_dbg_handler_custom(false);
-    
+
     // restore old callback
     SIM->set_notify_callback(old_callback, old_callback_arg);
+
 
   }
 
@@ -77,6 +158,11 @@
  */
 bx_dbg_gui_c::bx_dbg_gui_c(void) {
 
+  this->bCpuModeHasChanged = true;
+  this->currentCPUNo = 0;
+  this->CPUcount = BX_SMP_PROCESSORS;
+
+  this->init_register_refs();
 }
 
 /**
@@ -84,8 +170,158 @@ bx_dbg_gui_c::bx_dbg_gui_c(void) {
  */
 bx_dbg_gui_c::~bx_dbg_gui_c(void) {
 
+  if (this->smp_info.cpu_info != NULL) {
+    free(this->smp_info.cpu_info);
+  }
 }
 
+/**
+ * init_internal
+ */
+void bx_dbg_gui_c::init_internal(void) {
+
+
+  this->read_dbg_gui_config();
+
+}
+
+
+
+
+
+
+/**
+ * read_dbg_gui_config
+ */
+void bx_dbg_gui_c::read_dbg_gui_config(void) {
+
+  FILE * fd;
+  char line[512];
+  char * ret;
+  bool format_checked;
+
+  fd = fopen("bx_new_dbg.cfg", "r");
+  if (fd == NULL) return;
+
+  format_checked = false;
+  do {
+    ret = fgets(line, sizeof(line)-1, fd);
+    line[sizeof(line) - 1] = '\0';
+    size_t len = strlen(line);
+    if ((len>0) && (line[len-1] < ' ')) {
+      line[len-1] = '\0';
+    }
+    if ((ret != NULL) && (strlen(line) > 0)) {
+      if (!format_checked) {
+        if (!strncmp(line, "# bx_new_dbg_cfg", 16)) {
+          format_checked = true;
+        } else {
+          break;
+        }
+      } else {
+        char * param;
+        char * val;
+
+        if (line[0] == '#') {
+          continue;
+        }
+        param = strtok(line, "=");
+        if (param != NULL) {
+          param = strip_whitespace(param);
+          val = strtok(NULL, "");
+          if (val == NULL) {
+            fprintf(stderr, "bx_new_dbg.ini: missing value for parameter '%s'\n", param);
+            continue;
+          }
+          // here process the param and val
+
+
+          // last option involve parse_os_setting
+          if (!parse_os_setting(param, val)) {
+            fprintf(stderr, "bx_new_dbg.ini: unknown option '%s'\n", line);
+          }
+        } else {
+          continue;
+        }
+      }
+    }
+  } while (!feof(fd));
+  fclose(fd);
+
+}
+
+/**
+ * write_dbg_gui_config
+ */
+void bx_dbg_gui_c::write_dbg_gui_config(void) {
+
+  FILE *fd;
+
+  fd = fopen("bx_new_dbg.cfg", "w");
+  if (fd == NULL) return;
+  fprintf(fd, "# bx_new_dbg_cfg\n");
+
+  fprintf(fd, "# bx_new_dbg_cfg\n");
+  fclose(fd);
+
+}
+
+/**
+ * strip_whitespace
+ */
+char * bx_dbg_gui_c::strip_whitespace(char * s) {
+
+  char * first_valid;
+  char * last_valid;
+  size_t s_len;
+
+  first_valid = s;
+  s_len = strlen(s);
+  last_valid = s + ((s_len > 0) ? s_len-1 : 0);
+
+  while ((first_valid != last_valid) && (*first_valid == ' ' | *first_valid == '\t')) {
+    first_valid++;
+  }
+
+  while ((last_valid != first_valid) && (*last_valid == ' ' | *last_valid == '\t')) {
+    *last_valid = '\0';
+    last_valid--;
+  }
+
+  return (first_valid);
+
+}
+
+
+
+/**
+ * init_register_refs
+ */
+void bx_dbg_gui_c::init_register_refs(void) {
+
+  bx_list_c * cpu_list;
+
+  this->smp_info.cpu_count = BX_SMP_PROCESSORS;
+  this->smp_info.cpu_info = (bx_cpu_info_t *)malloc( BX_SMP_PROCESSORS * sizeof(bx_cpu_info_t));
+  memset(this->smp_info.cpu_info, 0, BX_SMP_PROCESSORS * sizeof(bx_cpu_info_t));
+  // now fill the bx_cpu_info_t struct each cpu
+  for (int cpuNo = 0; cpuNo<BX_SMP_PROCESSORS; cpuNo++) {
+
+    char cpuname[16];
+    int cpuregno;
+
+    snprintf(cpuname, 16, "bochs.cpu%d", cpuNo);
+    cpu_list = (bx_list_c *) SIM->get_param(cpuname, root_param);
+
+    // now fill the regs ...
+    cpuregno = 0;
+    while (dbg_regnames[cpuregno] != NULL) {
+      this->smp_info.cpu_info[cpuNo].regs[cpuregno] = SIM->get_param_num(dbg_regnames[cpuregno], cpu_list);
+      cpuregno++;
+    }
+
+  }
+}
 
 
 
--- bochs/gui/new_dbg.h
+++ bochs/gui/new_dbg.h
@@ -28,6 +28,14 @@
 
 #if BX_DEBUGGER && !BX_DEBUGGER_GUI && BX_NEW_DEBUGGER_GUI
 
+#include <stdio.h>
+#include "bochs.h"
+#include "siminterface.h"
+// #include "bx_debug/debug.h"
+// #include "memory/memory-bochs.h"
+// #include "pc_system.h"
+#include "cpu/cpu.h"
+
 #define IMPLEMENT_GUI_DEBUGGER(debugger_class) \
 debugger_class * bx_dbg_new; \
 void new_dbg_handler_custom(bool init) { \
@@ -38,15 +46,206 @@ void new_dbg_handler_custom(bool init) { \
   } \
 }
 
+#define DECLARE_GUI_DEBUGGER_VIRTUAL_METHODS()                                 \
+protected:                                                                     \
+  virtual void init_os_depended(void);                                         \
+
+
+
+#define DECLARE_GUI_DEBUGGER_OPTIONAL_VIRTUAL_METHODS()                        \
+  virtual bool parse_os_setting(const char * param, const char * value)        \
+  virtual void write_os_setting(FILE * fd)
+
+
+typedef enum {
+
+  RAX = 0,
+  RBX,
+  RCX,
+  RDX,
+  RSI,
+  RDI,
+  RBP,
+  RSP,
+  RIP,
+
+  EAX = RAX,  // only if someone will use them
+  EBX,
+  ECX,
+  EDX,
+  ESI,
+  EDI,
+  EBP,
+  ESP,
+  EIP,
+
+#if BX_SUPPORT_X86_64 == 1
+  R8,
+  R9,
+  R10,
+  R11,
+  R12,
+  R13,
+  R14,
+  R15,
+#endif /* BX_SUPPORT_X86_64 == 1 */
+
+  EFLAGS,
+  CS,
+  DS,
+  ES,
+  FS,
+  GS,
+
+  GDTR_BASE,
+  GDTR_LIMIT,
+  IDTR_BASE,
+  IDTR_LIMIT,
+  LDTR,
+  TR,
+
+  CR0,
+  CR2,
+  CR3,
+#if BX_CPU_LEVEL >= 5
+  CR4,
+#endif /* BX_CPU_LEVEL >= 5 */
+
+#if BX_CPU_LEVEL >= 6
+  MSR_EFER,
+#endif /* BX_CPU_LEVEL >= 6 */
+
+#if BX_SUPPORT_FPU
+  FPU_ST0_F,
+  FPU_ST0_E,
+  FPU_ST1_F,
+  FPU_ST1_E,
+  FPU_ST2_F,
+  FPU_ST2_E,
+  FPU_ST3_F,
+  FPU_ST3_E,
+  FPU_ST4_F,
+  FPU_ST4_E,
+  FPU_ST5_F,
+  FPU_ST5_E,
+  FPU_ST6_F,
+  FPU_ST6_E,
+  FPU_ST7_F,
+  FPU_ST7_E,
+#endif /* BX_SUPPORT_FPU */
+
+#if BX_CPU_TEST_REGISTER
+#if BX_CPU_LEVEL >= 3 && BX_CPU_LEVEL <= 6
+  TR3,
+  TR4,
+  TR5,
+#if BX_CPU_LEVEL >= 4
+  TR6,
+  TR7,
+#endif /* BX_CPU_LEVEL >= 4 */
+#endif /* BX_CPU_LEVEL >= 3 && BX_CPU_LEVEL <= 6 */
+#endif /* BX_CPU_TEST_REGISTER */
+
+#if BX_CPU_LEVEL >= 6
+
+  SSE_XMM00_0,
+  SSE_XMM01_0,
+  SSE_XMM02_0,
+  SSE_XMM03_0,
+  SSE_XMM04_0,
+  SSE_XMM05_0,
+  SSE_XMM06_0,
+  SSE_XMM07_0,
+#if BX_SUPPORT_X86_64
+  SSE_XMM08_0,
+  SSE_XMM09_0,
+  SSE_XMM10_0,
+  SSE_XMM11_0,
+  SSE_XMM12_0,
+  SSE_XMM13_0,
+  SSE_XMM14_0,
+  SSE_XMM15_0,
+#endif /* BX_SUPPORT_X86_64 */
+
+  SSE_XMM00_1,
+  SSE_XMM01_1,
+  SSE_XMM02_1,
+  SSE_XMM03_1,
+  SSE_XMM04_1,
+  SSE_XMM05_1,
+  SSE_XMM06_1,
+  SSE_XMM07_1,
+#if BX_SUPPORT_X86_64
+  SSE_XMM08_1,
+  SSE_XMM09_1,
+  SSE_XMM10_1,
+  SSE_XMM11_1,
+  SSE_XMM12_1,
+  SSE_XMM13_1,
+  SSE_XMM14_1,
+  SSE_XMM15_1,
+#endif /* BX_SUPPORT_X86_64 */
+
+#endif /* BX_CPU_LEVEL >= 6 */
+
+  DR0,
+  DR1,
+  DR2,
+  DR3,
+  DR6,
+  DR7,
+
+  CPU_REG_END
+} dbg_cpu_reg_t;
+
+typedef struct {
+  Bit64u            reg_value[CPU_REG_END];
+  Bit64u            reg_backup[CPU_REG_END];
+  bx_param_num_c *  regs[CPU_REG_END];
+} bx_cpu_info_t;
+
+typedef struct {
+  size_t          cpu_count;
+  bx_cpu_info_t * cpu_info;
+} bx_smp_info_t;
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// bx_dbg_gui_c
+////////////////////////////////////////////////////////////////////////////////
 class BOCHSAPI bx_dbg_gui_c {
 
 public:
   bx_dbg_gui_c(void);
   virtual ~bx_dbg_gui_c(void);
 
+  void init_internal(void);
+
 protected:
+  virtual void init_os_depended(void) {};
+  virtual bool parse_os_setting(const char * param, const char * value) { return false; };
+  virtual void write_os_setting(FILE * fd) {};
 
 private:
+  void read_dbg_gui_config(void);
+  void write_dbg_gui_config(void);
+  char * strip_whitespace(char * s);
+
+  void init_register_refs(void);
+
+  bx_smp_info_t smp_info;
+
+
+
+  bool bCpuModeHasChanged;  // set if cpu mode has changed
+  unsigned currentCPUNo;    // active cpu
+  bool showAllSMPCPUs;      // Display all SMP CPUs
+  unsigned CPUcount;        // # of CPUs in a multi-CPU simulation
+
 
 };
 
